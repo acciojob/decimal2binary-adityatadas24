d4b96f1e392b586a7e16ef64a0238080681d6a53
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-22 12:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	 if (decimal === 0) {
+        return '0';
+    }
+    let binary = '';
+    while (decimal > 0) {
+        binary = (decimal % 2) + binary;
+        decimal = Math.floor(decimal / 2);
+    }
+    return binary;
+}
+
   
 }
 
